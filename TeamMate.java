86e0a5f1f25495008a48e64fc566e2dc0ca0a645
AlejandroBrites <104328707+AlejandroBrites@users.noreply.github.com>
2022-09-29 09:58:06 -0300

    Create TeamMate.java

---
+++ TeamMate.java
@@ -0,0 +1,58 @@
+class TeamMate {
+  
+  private String userId;
+  private String name;
+  private boolean online;
+
+  public TeamMate() {
+  }
+
+  public TeamMate (String userId, String name, boolean online) {
+    
+    this.userId = userId;
+    this.name = name;
+    this.online = online;
+    
+  }
+
+  public void setTodos(String userId, String name, boolean online) {
+    
+    this.userId = userId;
+    this.name = name;
+    this.online = online;
+    
+  }
+
+  public void setUsuario (String userId, String name) {
+
+    this.userId = userId;
+    this.name = name;
+    this.online = false;
+    
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getUserId() {
+    return this.userId;
+  }
+
+  public boolean getOnline() {
+    return this.online;
+  }
+
+  public void setUserId (String userId) {
+    this.userId = userId;
+  }
+
+  public void setName (String name) {
+    this.name = name;
+  }
+
+  public void setOnline (boolean online) {
+    this.online = online;
+  }
+
+}
